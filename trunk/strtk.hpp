4ddfd532b95229353aa71156acfbbbec9d330b7e
partow@gmail.com <partow@gmail.com@22d9a970-3041-0410-883d-43345ed541d8>
2014-02-11 09:19:20 +0000

    C++ String Toolkit Library (StrTk)  http://www.partow.net/programming/strtk/index.html

--- trunk/strtk.hpp
+++ trunk/strtk.hpp
@@ -23630,7 +23630,7 @@ namespace strtk
    {
       static const char* library = "String Toolkit";
       static const char* version = "2.718281828459045235360287471352662497757247093699959574";
-      static const char* date    = "20130630";
+      static const char* date    = "20140118";
 
       static inline std::string data()
       {
